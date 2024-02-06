d69e1daaefd8b2e643c606a57f33f438c99a4ef5
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-02-06 21:38:02 +0200

    css-modules

--- src/componenets/AppNav.jsx
+++ src/componenets/AppNav.jsx
@@ -1,6 +1,7 @@
+import style from './AppNav.module.css'
 export default function AppNav() {
     return (
-        <nav>
+        <nav className={style.nav}>
             App navigation
         </nav>
     )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 
 import { Link } from "react-router-dom";
 import PageNav from "../componenets/PageNav";
+import AppNav from "../componenets/AppNav";
 export default function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>WorldWise</h1>
       {/* <a href="/Pricing">Pricing</a>  */}
       <Link to="/app">Go to the App</Link>
