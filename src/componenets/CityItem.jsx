039436ee0e4dd539449574bde678be295fe158c4
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-02-11 16:24:46 +0200

    cityItem-btn

--- src/componenets/CityItem.jsx
+++ src/componenets/CityItem.jsx
@@ -1,7 +1,19 @@
-export default function CityItem() {
-    return (
-        <li>
-            city-city
-        </li>
-    )
+import styles from "./CityItem.module.css";
+export default function CityItem({ city }) {
+  const{cityName, emoji, date} = city;
+
+  const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    
+  }).format(new Date(date));
+
+  return <li className={styles.cityItem}>
+    <span className={styles.emoji}>{emoji}</span>
+    <h3 className={styles.name}>{cityName}</h3>
+    <time className={styles.date}>{formatDate(date)}</time>
+    <button className={styles.deleteBtn}>&times;</button>
+  </li>;
 }
