15317236855548c043f5424c078c45af075ebb32
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-02-12 20:49:16 +0200

    countries2

--- src/componenets/CountryList.jsx
+++ src/componenets/CountryList.jsx
@@ -1,14 +1,19 @@
 import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
-import CityItem from "./CityItem";
+import CountryItem from "./CountryItem";
 import Message from "./Message";
 
 export default function CountriesList({ cities, isLoading }) {
     if (isLoading) return <Spinner />;
-    if(!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>
-      const countries=[];
+    if(!cities.length) return <Message message="Add your first country by clicking on a country on the map"/>
+    console.log(cities);
+      const countries=cities.reduce((arr, city)=>{
+        if(!arr.map((el)=>el.country).includes(city.country))
+        return [...arr, {country: city.country, emoji:city.emoji}];
+      else return arr;
+      }, []);
 
     return <ul className={styles.countryList}>
-      {cities.map(city=> <CityItem city = {city} key={city.id} />)}
+      {countries.map(country=> <CountryItem country = {country}  />)}
       </ul>;
   }
