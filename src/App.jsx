dc2a72e548b9d249b9dc635d82aabe9cdcb319a8
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-02-13 17:24:18 +0200

    Navigte-replace

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import Product from "./pages/Product";
@@ -41,7 +41,8 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route index element={<Navigate replace 
+          to="cities"/>} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
 
           <Route path = "cities/:id" element={<City />}/>
