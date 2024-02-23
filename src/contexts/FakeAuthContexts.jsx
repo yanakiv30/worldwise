ba50bd76f350c73279c78c4fecf723d7d5632a13
yanakiv30 <yanakiv30@gmail.com>
2024-02-23 17:26:03 +0200

    AuthContext

---
+++ src/contexts/FakeAuthContexts.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({children}){
+    return <AuthContext.Provider>{children}</AuthContext.Provider>
+}
+
+function useAuth(){
+    const context = useContext(AuthContext)
+    if (context===undefined) throw new Error("AuthContext was used outside AuthProvider")
+}
