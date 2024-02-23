48e091ed5ab1232f23d2f8888060258da2d5990f
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-02-23 19:04:52 +0200

    reducer

--- src/contexts/FakeAuthContexts.jsx
+++ src/contexts/FakeAuthContexts.jsx
@@ -1,12 +1,23 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
+const initialState={user:null,isAthenticated:false};
+function reducer(state,action){
+    
+}
 
 function AuthProvider({children}){
+
+    const [{user, isAthenticated},dispatch] = useReducer(reducer, initialState)
+function login(email,password) {};
+
+function logout() {}
+
     return <AuthContext.Provider>{children}</AuthContext.Provider>
 }
 
 function useAuth(){
-    const context = useContext(AuthContext)
-    if (context===undefined) throw new Error("AuthContext was used outside AuthProvider")
+    const contextValue = useContext(AuthContext)
+    if (contextValue===undefined) throw new Error("AuthContext was used outside AuthProvider");
+    return contextValue
 }
