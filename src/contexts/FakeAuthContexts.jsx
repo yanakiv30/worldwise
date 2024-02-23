c9e93075b7a7df3916d9933328d2f209cf1f96c8
yanakiv30 <yanakiv30@gmail.com>
2024-02-23 19:48:35 +0200

    export

--- src/contexts/FakeAuthContexts.jsx
+++ src/contexts/FakeAuthContexts.jsx
@@ -1,23 +1,46 @@
 import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
-const initialState={user:null,isAthenticated:false};
-function reducer(state,action){
-    
+const initialState = { user: null, isAthenticated: false };
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return { ...state, user: action.payload, isAthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAthenticated: false };
+      default:
+        throw new Error("Unknown action");
+  }
 }
 
-function AuthProvider({children}){
+const FAKE_USER = {
+    name: "Jack",
+    email: "jack@example.com",
+    password: "qwerty",
+    avatar: "https://i.pravatar.cc/100?u=zz",
+  };
+  
 
-    const [{user, isAthenticated},dispatch] = useReducer(reducer, initialState)
-function login(email,password) {};
+function AuthProvider({ children }) {
+  const [{ user, isAthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  function login(email, password) {
+    if(email===FAKE_USER.email && password===FAKE_USER.password) dispatch({type:"login", payload:FAKE_USER});
+  }
 
-function logout() {}
+  function logout() {
+    dispatch({type:"logout"});
+  }
 
-    return <AuthContext.Provider>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={{user,isAthenticated,login,logout}}>{children}</AuthContext.Provider>;
 }
 
-function useAuth(){
-    const contextValue = useContext(AuthContext)
-    if (contextValue===undefined) throw new Error("AuthContext was used outside AuthProvider");
-    return contextValue
+function useAuth() {
+  const contextValue = useContext(AuthContext);
+  if (contextValue === undefined)
+    throw new Error("AuthContext was used outside AuthProvider");
+  return contextValue;
 }
+export {AuthProvider, useAuth};
